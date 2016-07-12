7c7a81b28c3cc378f8894216aab9bd8cc206e895
Vera Patricio <vera@calends1.univ-lyon1.fr>
2016-07-12 15:13:17 +0200

    Cosmetic changes: rowcol is now a function

--- rowcol.py
+++ rowcol.py
@@ -2,25 +2,41 @@ from mpdaf.obj import Cube,Image,iter_ima
 import numpy as np
 import multiprocessing
 
-c=Cube('DATACUBE_MACS0416_ZAP_26Jan_MAD_ZAP_median_GOOD_VAR.fits')
-#immask=Image('MASK_ZAP_MACS0416_ext1_24Aug.fits')
-immask=Image('MASKINV.fits')
+def remove_bg(cube_in,mask,cube_out):
+	""" Corrects background of a cube to zero (per wavelengh plane)
+	Strong emission (e.g. cluster members) should be masked.
+	Parameters:
+	----------
+	cube_in: string
+		path to input cube to be corrected
+	mask:	 string
+		path to mask to be used
+	cube_out: string
+		path to the output (corrected) cube
+	Returns:
+	----------
+	mpdaf.obj.Cube 
+	corrected cube
+	"""
 
-c2=c.copy()
+	c=Cube(cube_in)
+	immask = Image(mask)
 
-mask = immask.data.astype(bool)
-c.data.mask[:, mask] = True
+	c2=c.copy()
+	mask = immask.data.astype(bool)
+	c.data.mask[:, mask] = True
 
-for k in range(c.shape[0]):
-    print k," out of ",c.shape[0]
-    posmin=max(k-25,0)
-    posmax=min(c.shape[0],k+25)
-    for p in range(c.shape[1]):
-        med=np.ma.median(c.data[posmin:posmax,p,:])
-        c2.data.data[k,p,:]-=med
-    for q in range(c.shape[2]):
-        med=np.ma.median(c.data[posmin:posmax,:,q])
-        c2.data.data[k,:,q]-=med
+	for k in range(c.shape[0]):
+    		print k," out of ",c.shape[0]
+    		posmin=max(k-25,0)
+    		posmax=min(c.shape[0],k+25)
+   		for p in range(c.shape[1]):
+        		med=np.ma.median(c.data[posmin:posmax,p,:])
+        		c2.data.data[k,p,:]-=med
+    		for q in range(c.shape[2]):
+        		med=np.ma.median(c.data[posmin:posmax,:,q])
+        		c2.data.data[k,:,q]-=med
 
-
-c2.write('DATACUBE_MACS0416_ZAP_26Jan_MAD_ZAP_median_GOOD_VAR_rowcol.fits')
+	c2.write(cubeout)
+	
+	return c2
