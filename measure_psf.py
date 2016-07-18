729c9dccf39639d6449c1948ae09276c4928694b
VeraPatricio <vera.patricio@univ-lyon1.fr>
2016-07-18 14:23:24 +0200

    Added measure_psf, small script to measure the psf in a cube

---
+++ measure_psf.py
@@ -0,0 +1,130 @@
+from mpdaf.obj import Cube,Image
+import argparse
+import numpy as np
+import matplotlib.pylab as plt
+
+def measure_psf(im,ra,dec):
+	""" Fits a gaussian and moffat 2D profile in image 'im' centered in p,q coordinates.
+	of each object to be measured.
+	The output values of all objects are averaged (after a sigma clipping ti get rid
+	of obvious bad fits)
+	Parameters:
+	----------
+	im: mpdaf.obj.Image	
+		image where to measure the psf
+	ra:	float
+		array of righ ascensions of objects to be measured 
+	dec:	float
+		array of righ declination to be measured 
+	Returns:
+	--------
+	fwhm,n,gfwhm
+		full width half maximum of the moffat and its std deviation
+		n of the moffat	 and its std
+		full width half maximum of the gaussian and its std
+	"""
+
+        fwhm = []
+        gfwhm = []
+        n = []
+
+	#### TESTING ####
+	plt.figure()
+	im.plot(zscale=True)
+
+        ## Fit all the objects
+        for r,d in zip(ra,dec):
+                gfit = im.gauss_fit(center=(d,r),pos_min=(d-2,r-2),pos_max=(d+2,r+2),circular=True,verbose=0,plot=True)
+                gfwhm.append(gfit.fwhm[0])
+		try:
+                	fit = im.moffat_fit(center=gfit.center,pos_min=(d-2,r-2),pos_max=(d+2,r+2),circular=True,verbose=0)
+                	n.append(fit.n)
+                	fwhm.append(fit.fwhm[0])
+		except RuntimeWarning:
+                	n.append(np.nan)
+                	fwhm.append(np.nan)
+	
+        ## Get rid of the bad ones and make an average
+        mean_gfwhm = np.nanmean(gfwhm)
+        std_gfwhm = np.nanstd(gfwhm)
+        mean_fwhm = np.nanmean(fwhm)
+        std_fwhm = np.nanstd(fwhm)
+        mean_n = np.nanmean(n)
+        std_n = np.nanstd(n)
+	
+	print('Finished star loop')
+        print(mean_gfwhm,std_gfwhm,mean_fwhm,std_fwhm,mean_n,std_n)
+
+        return (mean_gfwhm,std_gfwhm,mean_fwhm,std_fwhm,mean_n,std_n)
+
+
+if __name__ == "__main__":
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("cube", type=str, help='Cube to be measured')
+        parser.add_argument("star_list",type=str, help = 'List of stars coordinates to be measured (Nb ra dec in degrees)')
+        args = parser.parse_args()
+
+        if len(vars(args)) < 2:
+                print 'Missing argument'
+                print 'Usage: python measure_psf.py cube star_list'
+                sys.exit()
+
+	## Read star list and open output results
+	id,dec,ra = np.loadtxt(args.star_list,unpack=True)
+	f = open('psf_results.dat','w')
+	f.write('Wavelengh (A)	Gaussian FWHM (")	Moffat n/beta (") 	Moffat fwhm (") 	Moffat alpha(")\n')
+	f.write('--------------------------------------------------------------------------------------------------------\n')
+	
+	## Load cube and loop over the several wavelengh planes
+	c = Cube(args.cube)
+	c = c.rebin_mean((2*368,1,1))
+	sample_fwhm = []
+	sample_gfwhm= []
+	sample_n = []
+	sample_alpha = []
+	white = c.sum(axis=0)
+	(gfwhm,std_gfwhm,fwhm,std_fwhm,n,std_n) = measure_psf(white,ra,dec)
+	alpha = fwhm / (2 *np.sqrt(2 * (1/n) - 1))
+        err_alpha = std_fwhm / (2 *np.sqrt(2 * (1/n) - 1)) + std_n * fwhm / (2 * np.sqrt(2/n- n*n))
+                        ## ugly derivative with Wolfram Alpha
+	f.write('white light  %0.2f +/- %0.2f         %0.2f +/- %0.2f         %0.2f +/- %0.2f         %0.2f +/- %0.2f \n'
+                        %(gfwhm,std_gfwhm,n,std_n,fwhm,std_fwhm, alpha,err_alpha))
+	f.write('--------------------------------------------------------------------------------------------------------\n')
+		
+
+	for k in range(0,c.shape[0]):
+		print('Measusing %s out of 5 slices'%k)
+		im = c[k]
+		im.wcs = c.wcs ## Not elegant, but the wcs is not keept when you only select one slice
+		(gfwhm,std_gfwhm,fwhm,std_fwhm,n,std_n) = measure_psf(im,ra,dec)
+		
+		## Convert mpdaf fwhm in proper moffat parameters
+        	## https://en.wikipedia.org/wiki/Moffat_distribution
+        	## n == beta
+        	## alpha = fwhm / (2 * sqrt (2 ** (1/n) -1))
+       	 	alpha = fwhm / (2 *np.sqrt(2 * (1/n) - 1))
+        	err_alpha = std_fwhm / (2 *np.sqrt(2 * (1/n) - 1)) + std_n * fwhm / (2 * np.sqrt(2/n- n*n)) 
+
+		f.write('%d 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f \n'
+			%(c.wave.coord(k),gfwhm,std_gfwhm,n,std_n,fwhm,std_fwhm, alpha,err_alpha))
+		
+		## Keep the values to calculate mean and error
+		sample_fwhm.append(fwhm)
+		sample_gfwhm.append(gfwhm)
+		sample_n.append(n)
+		sample_alpha.append(alpha)
+
+	print('Finished slices loop')
+		
+	f.write('--------------------------------------------------------------------------------------------------------\n')
+	sqrt_nb = np.sqrt(len(sample_fwhm))
+	f.write('mean	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f \n'
+			%(np.mean(sample_gfwhm),np.std(sample_gfwhm)/sqrt_nb,
+			  np.mean(sample_n),np.std(sample_n)/sqrt_nb,
+			  np.mean(sample_fwhm),np.std(sample_fwhm)/sqrt_nb,
+			  np.mean(sample_alpha),np.std(sample_alpha)/sqrt_nb))
+
+	f.close()	
+
+
