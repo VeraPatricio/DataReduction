e41a13900695f86a742e4749379d0b1bbabbafd1
VeraPatricio <vera.patricio@univ-lyon1.fr>
2016-07-20 15:42:20 +0200

    Changed fit to used pixel coordinates

--- measure_psf.py
+++ measure_psf.py
@@ -3,6 +3,8 @@ import argparse
 import numpy as np
 import matplotlib.pylab as plt
 
+radii = 5 ## pix
+
 def measure_psf(im,ra,dec):
 	""" Fits a gaussian and moffat 2D profile in image 'im' centered in p,q coordinates.
 	of each object to be measured.
@@ -32,18 +34,44 @@ def measure_psf(im,ra,dec):
 	plt.figure()
 	im.plot(zscale=True)
 
-        ## Fit all the objects
-        for r,d in zip(ra,dec):
-                gfit = im.gauss_fit(center=(d,r),pos_min=(d-2,r-2),pos_max=(d+2,r+2),circular=True,verbose=0,plot=True)
-                gfwhm.append(gfit.fwhm[0])
-		try:
-                	fit = im.moffat_fit(center=gfit.center,pos_min=(d-2,r-2),pos_max=(d+2,r+2),circular=True,verbose=0)
-                	n.append(fit.n)
-                	fwhm.append(fit.fwhm[0])
-		except RuntimeWarning:
-                	n.append(np.nan)
-                	fwhm.append(np.nan)
+	if np.isscalar(dec): 
 	
+		## I was having some troubles with gaussian/moffat fitting using ra,dec coordinates,
+        	## and since mpdaf converts it internally, I prefer to do the converstion here.
+	
+        	(p,q) = im.wcs.sky2pix((dec,ra))[0]
+
+		 ## Only one star (not a great idea for real science...)
+                gfit = im.gauss_fit(center=(p,q),pos_min=(p-radii,q-radii),pos_max=(p+radii,q+radii),
+			circular=True,verbose=0,unit_center=None,plot=True) ## FWHM will still be outputed in arcsec
+                gfwhm.append(gfit.fwhm[0])
+                try:
+                        fit = im.moffat_fit(center=gfit.center,pos_min=(p-radii,q-radii),pos_max=(p+radii,q+radii),
+				unit_center=None,circular=True,verbose=0)
+                        n.append(fit.n)
+                        fwhm.append(fit.fwhm[0])
+                except RuntimeWarning:
+                        n.append(np.nan)
+                        fwhm.append(np.nan)
+
+	else:	
+        	## Fit all the objects
+        	for r,d in zip(ra,dec):
+			(p,q) = im.wcs.sky2pix((d,r))[0]
+
+                	gfit = im.gauss_fit(center=(d,r),pos_min=(d-radii,r-radii),pos_max=(d+radii,r+radii),
+				unit_center=None,circular=True,verbose=0,plot=True)
+               		gfwhm.append(gfit.fwhm[0])
+			try:
+                		fit = im.moffat_fit(center=gfit.center,pos_min=(d-radii,r-radii),pos_max=(d+radii,r+radii),
+					unit_center=None,circular=True,verbose=0)
+                		n.append(fit.n)
+                		fwhm.append(fit.fwhm[0])
+			except RuntimeWarning:
+                		n.append(np.nan)
+                		fwhm.append(np.nan)
+		
+
         ## Get rid of the bad ones and make an average
         mean_gfwhm = np.nanmean(gfwhm)
         std_gfwhm = np.nanstd(gfwhm)
@@ -70,13 +98,14 @@ if __name__ == "__main__":
                 print 'Usage: python measure_psf.py cube star_list'
                 sys.exit()
 
-	## Read star list and open output results
-	id,dec,ra = np.loadtxt(args.star_list,unpack=True)
 	f = open('psf_results.dat','w')
 	f.write('Wavelengh (A)	Gaussian FWHM (")	Moffat n/beta (") 	Moffat fwhm (") 	Moffat alpha(")\n')
 	f.write('--------------------------------------------------------------------------------------------------------\n')
+
+	## Read star list and open output results
+        id,dec,ra = np.loadtxt(args.star_list,unpack=True)
 	
-	## Load cube and loop over the several wavelengh planes
+	## Load cube and bin 
 	c = Cube(args.cube)
 	c = c.rebin_mean((2*368,1,1))
 	sample_fwhm = []
@@ -88,7 +117,7 @@ if __name__ == "__main__":
 	alpha = fwhm / (2 *np.sqrt(2 * (1/n) - 1))
         err_alpha = std_fwhm / (2 *np.sqrt(2 * (1/n) - 1)) + std_n * fwhm / (2 * np.sqrt(2/n- n*n))
                         ## ugly derivative with Wolfram Alpha
-	f.write('white light  %0.2f +/- %0.2f         %0.2f +/- %0.2f         %0.2f +/- %0.2f         %0.2f +/- %0.2f \n'
+	f.write('white light \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \n'
                         %(gfwhm,std_gfwhm,n,std_n,fwhm,std_fwhm, alpha,err_alpha))
 	f.write('--------------------------------------------------------------------------------------------------------\n')
 		
@@ -106,7 +135,7 @@ if __name__ == "__main__":
        	 	alpha = fwhm / (2 *np.sqrt(2 * (1/n) - 1))
         	err_alpha = std_fwhm / (2 *np.sqrt(2 * (1/n) - 1)) + std_n * fwhm / (2 * np.sqrt(2/n- n*n)) 
 
-		f.write('%d 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f \n'
+		f.write('%d \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \n'
 			%(c.wave.coord(k),gfwhm,std_gfwhm,n,std_n,fwhm,std_fwhm, alpha,err_alpha))
 		
 		## Keep the values to calculate mean and error
@@ -119,7 +148,7 @@ if __name__ == "__main__":
 		
 	f.write('--------------------------------------------------------------------------------------------------------\n')
 	sqrt_nb = np.sqrt(len(sample_fwhm))
-	f.write('mean	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f 	%0.2f +/- %0.2f \n'
+	f.write('mean \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \t %0.2f +/- %0.2f \n'
 			%(np.mean(sample_gfwhm),np.std(sample_gfwhm)/sqrt_nb,
 			  np.mean(sample_n),np.std(sample_n)/sqrt_nb,
 			  np.mean(sample_fwhm),np.std(sample_fwhm)/sqrt_nb,
