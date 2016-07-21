c0250b859e0fa37d5160765f001a6c3aea304c55
Vera Patricio <vera.patricio@univ-lyon1.fr>
2016-07-21 15:18:06 +0200

    Added parallelization

--- rowcol.py
+++ rowcol.py
@@ -1,7 +1,24 @@
 from mpdaf.obj import Cube,Image,iter_ima
 import numpy as np
-import multiprocessing
+from multiprocessing import Process
+#from multiprocessing import Pool
 import argparse
+import time
+
+def rm_bg_multiproc(k,cube_in,cube_out):
+
+        posmin=max(k-25,0)
+        posmax=min(c.shape[0],k+25)
+        for p in range(c.shape[1]):
+        	med=np.ma.median(c.data[posmin:posmax,p,:])
+                c2.data.data[k,p,:]-=med
+        for q in range(c.shape[2]):
+                med=np.ma.median(c.data[posmin:posmax,:,q])
+         	c2.data.data[k,:,q]-=med
+
+def dummy_function(i):
+	return rm_bg_multiproc(i,c,c2)
+
 
 def remove_bg(cube_in,mask,cube_out):
 	""" Corrects background of a cube to zero (per wavelengh plane)
@@ -29,9 +46,9 @@ def remove_bg(cube_in,mask,cube_out):
 	mask_inv = np.invert(mask)
 	c.data.mask[:, mask_inv] = True
 
+	tstart = time.time()
 	for k in range(c.shape[0]):
-    		print k," out of ",c.shape[0]
-    		posmin=max(k-25,0)
+		posmin=max(k-25,0)
     		posmax=min(c.shape[0],k+25)
    		for p in range(c.shape[1]):
         		med=np.ma.median(c.data[posmin:posmax,p,:])
@@ -39,10 +56,11 @@ def remove_bg(cube_in,mask,cube_out):
     		for q in range(c.shape[2]):
         		med=np.ma.median(c.data[posmin:posmax,:,q])
         		c2.data.data[k,:,q]-=med
+	tend = time.time()
+        print('ellapsed time %s'%(tend-tstart))
 
 	## Update Header
 	c2.primary_header.add_comment('This cube has been median subtracted')
-
 	c2.write(cube_out)
 	
 	return c2
@@ -53,14 +71,46 @@ if __name__ == "__main__":
         parser.add_argument("cube_in", type=str, help='path to input cube to be corrected')
         parser.add_argument("mask",type=str, help = 'path to mask to be used')
         parser.add_argument("cube_out", type=str, help = 'path to the output (corrected) cube')
+	parser.add_argument("multip",type=int,help='Uses python multiprocessing (1) or not (0)')
         args = parser.parse_args()
-
-        if len(vars(args)) < 3:
+	
+        if len(vars(args)) < 4:
 
                 print 'Missing argument'
-                print 'Usage: python rowcol.py cube_in mask cube_out'
+                print 'Usage: python rowcol.py cube_in mask cube_out multip'
                 sys.exit()
 
 	
-	remove_bg(args.cube_in,args.mask,args.cube_out)
+	if args.multip == 1:
+
+		print('Using multiprocessing')	
+		c=Cube(args.cube_in)
+        	immask = Image(args.mask)
+
+        	c2=c.copy()
+        	mask = immask.data.astype(bool)
+        	mask_inv = np.invert(mask)
+        	c.data.mask[:, mask_inv] = True
+	
+		tstart = time.time()
+		#pool = Pool(processes=4)
+		#pool.map(dummy_function, range(c.shape[0]))     
+                for i in range(0,c.shape[0]):   
+                        p = Process(target=rm_bg_multiproc, args=(i,c,c2))
+                        p.start()
+
+		tend = time.time()
+                print('ellapsed time %s'%(tend-tstart))
+
+		## Update Header
+        	c2.primary_header.add_comment('This cube has been median subtracted')
+		c2.write(args.cube_out)
+
+	if args.multip == 0:
+
+		remove_bg(args.cube_in,args.mask,args.cube_out)
+
+	else:
+		print '1 or 0 for multip option'
+
 
