09f51f557dd43ed341b1d257273ae32ec40b5664
VeraPatricio <vera.patricio@univ-lyon1.fr>
2016-08-22 14:02:08 +0200

    Changed reading mode so that the order is the number on the file

--- make_fkedit.py
+++ make_fkedit.py
@@ -6,32 +6,13 @@ import sys
 import re
 from mpdaf.obj import Image
 
-### Anonying functions to sort files in a human-friendy way
-def tryint(s):
-    try:
-        return int(s)
-    except:
-        return s
-    
-def alphanum_key(s):
-    """ Turn a string into a list of string and number chunks.
-        "z23a" -> ["z", 23, "a"]
-    """
-    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
-
-def sort_nicely(l):
-    """ Sort the given list in the way that humans expect.
-    """
-    l.sort(key=alphanum_key)
-#######
-
-def make_fkedit_file(prefix_original):
+def make_fkedit_file(prefix):
 	""" Calculates the offsets to be applied from the scamp output and 
 	makes the corresponding fkedit file. A 'cata' folder with the 
-	IMAGESX.head output is required.
+	IMAGESX.head output is required. It assumes that prefix_X.fits --> IMAGE_X.head
 	Parameters:
 	----------
-	prefix_original:	str
+	prefix:	str
 		prefix of the original images (example: IMAGE_FOV_SKYSUB_)
 	Output:
 	-------
@@ -39,25 +20,20 @@ def make_fkedit_file(prefix_original):
 	"""
 
 	## Create auxiliary files with only the information needed
+	os.system('ls  cata/IMAGE*head | grep -o "[0-9]*" > image_order')
 	os.system('cat cata/IMAGE*.head | grep CRVAL1 >crval1')
 	os.system('cat cata/IMAGE*.head | grep CRVAL2 >crval2')
 
-	## Read the auxiliary files (yes it's dummy to do it like this, but it's faster
-	## to code...)
-	fcrval1 = open('crval1','r')
-	fcrval2 = open('crval2','r')
-
 	## output the results
 	f = open('dofkedit','w')
+	image_number = np.loadtxt('image_order')
 	crval1 = np.genfromtxt('crval1')
 	crval1 = crval1[~np.isnan(crval1)] ## Clean from nan
 	crval2 = np.genfromtxt('crval2')
 	crval2 = crval2[~np.isnan(crval2)] ## Clean from nan
 
-	imlist = glob.glob(prefix_original+'*fits')
-	sort_nicely(imlist)
-	for i,imname in enumerate(imlist):
-		im = Image(imname)
+	for i,im_nb in enumerate(image_number):
+		im = Image(prefix+str(int(im_nb))+'.fits')
 		good_ra = im.primary_header['RA']+ crval1[i] - im.data_header['CRVAL1']
 		f.write('fkedit -m RA -- %s PIXTABLE_REDUCED_%s.fits\n'%(good_ra,i+1))
 
@@ -68,23 +44,21 @@ def make_fkedit_file(prefix_original):
 			f.write('fkedit -m DEC %s PIXTABLE_REDUCED_%s.fits\n'%(good_dec,i+1))
 
 	f.close()
-	os.system('rm crval1 crval2')
+	os.system('rm crval1 crval2 image_order')
 		
 		
-
-
 if __name__ == "__main__":
 
         parser = argparse.ArgumentParser()
-        parser.add_argument("prefix_original", type=str, help='prefix of the original images (example: IMAGE_FOV_SKYSUB_)')
+        parser.add_argument("prefix", type=str, help='prefix of the original images (example: IMAGE_FOV_SKYSUB_)')
         args = parser.parse_args()
 
         if len(vars(args)) < 1:
 
                 print 'Missing argument'
-                print 'Usage: python make_fkedit.py prefix_original'
+                print 'Usage: python make_fkedit.py prefix'
                 sys.exit()
 
 	else:
 		
-		make_fkedit_file(args.prefix_original)
+		make_fkedit_file(args.prefix)
